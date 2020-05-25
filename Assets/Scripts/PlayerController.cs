d8d476dcc5ddaa13b22f9931f2d636b2f341fd99
VishnuRameshbabu <vishnuramesh200@gmail.com>
2020-05-25 14:06:43 +0530

    Fixed Player movement by mouse rotation. Added Pickups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
     public float verticalInput;
     //public Rigidbody playerRb;
     public float speed = 1f;
-    public float turnSpeed = 50f;
+    public float turnSpeed = 10f;
     
     // Start is called before the first frame update
     void Start()
@@ -16,20 +16,17 @@ public class PlayerController : MonoBehaviour
         //playerRb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void playerMovement() {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        float turnHorizontal, turnVertical;
+        float turnHorizontal;
         turnHorizontal = turnSpeed * Input.GetAxis("Mouse X");
-        turnVertical = turnSpeed * Input.GetAxis("Mouse Y");
-        transform.Rotate(0, turnVertical, 0);
+        transform.Rotate(Vector3.up, turnHorizontal * turnSpeed * Time.deltaTime); ;
         //playerRb.AddForce(transform.forward * verticalInput * Time.deltaTime * speed );
         if ((Input.GetKey(KeyCode.UpArrow)) || (Input.GetKey(KeyCode.W)))
         {
             transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
-            
+
         }
         if ((Input.GetKey(KeyCode.DownArrow)) || (Input.GetKey(KeyCode.S)))
         {
@@ -46,6 +43,32 @@ public class PlayerController : MonoBehaviour
             transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
 
         }
+
+    }
+
+    // Update is called once per frame
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "Lantern")
+        {
+            GameObject lantern = other.gameObject;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                lantern.transform.SetParent(transform);
+                lantern.transform.position = transform.position;
+                lantern.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+            }
+        }
         
     }
+    void DeactivateChild()
+    {
+      
+         transform.Find("Lantern_picked").gameObject.SetActive(false);    }  //Deactivates lantern after picking up but remains a child to the player
+                                                                                                    
+    void Update()
+    {
+        playerMovement();
+        DeactivateChild();
+}
 }
